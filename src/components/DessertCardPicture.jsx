4b7d5de13161740bdff7c3bc363d504c67bc7623
PaulDev <13600558+dev-paulL@users.noreply.github.com>
2024-08-06 12:37:10 +0200

    adjusted fonts and weights and some other things, added footer

--- src/components/DessertCardPicture.jsx
+++ src/components/DessertCardPicture.jsx
@@ -6,6 +6,7 @@ export default function DessertCardPicture({ dessert, quantityInCart }) {
       <source srcSet={dessert.image.desktop} media="(min-width: 64rem)" />
       <source srcSet={dessert.image.tablet} media="(min-width: 48rem)" />
       <img
+        loading="lazy"
         alt={dessert.name}
         src={dessert.image.mobile}
         className={`section_desserts-article--img ${
